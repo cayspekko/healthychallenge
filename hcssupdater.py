44bc66328c0a11be094d3534fa23b7d0e704e8c4
doug <doug@applegate.us>
2017-03-04 15:51:50 -0700

    change credential type

--- hcssupdater.py
+++ hcssupdater.py
@@ -11,6 +11,7 @@ from apiclient import discovery
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
+from oauth2client.service_account import ServiceAccountCredentials
 
 try:
     import argparse
@@ -22,7 +23,7 @@ except ImportError:
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
 SCOPES = 'https://www.googleapis.com/auth/spreadsheets'
 CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Google Sheets API Python Quickstart'
+APPLICATION_NAME = 'Healthy Challenge Updater'
 
 
 class HCSSUpdater(object):
@@ -43,23 +44,29 @@ class HCSSUpdater(object):
         Returns:
             Credentials, the obtained credential.
         """
-        home_dir = os.path.expanduser('~')
-        credential_dir = os.path.join(home_dir, '.credentials')
-        if not os.path.exists(credential_dir):
-            os.makedirs(credential_dir)
-        credential_path = os.path.join(credential_dir,
-                                       'sheets.googleapis.com-python-quickstart.json')
+        # home_dir = os.path.expanduser('~')
+        # credential_dir = os.path.join(home_dir, '.credentials')
+        # if not os.path.exists(credential_dir):
+        #     os.makedirs(credential_dir)
+        # credential_path = os.path.join(credential_dir,
+        #                                'sheets.googleapis.com-python-quickstart.json')
+        #
+        # store = Storage(credential_path)
+        # credentials = store.get()
+        # if not credentials or credentials.invalid:
+        #     flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        #     flow.user_agent = APPLICATION_NAME
+        #     if flags:
+        #         credentials = tools.run_flow(flow, store, flags)
+        #     else:  # Needed only for compatibility with Python 2.6
+        #         credentials = tools.run(flow, store)
+        #     print('Storing credentials to ' + credential_path)
 
-        store = Storage(credential_path)
-        credentials = store.get()
-        if not credentials or credentials.invalid:
-            flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-            flow.user_agent = APPLICATION_NAME
-            if flags:
-                credentials = tools.run_flow(flow, store, flags)
-            else:  # Needed only for compatibility with Python 2.6
-                credentials = tools.run(flow, store)
-            print('Storing credentials to ' + credential_path)
+        scopes = ['https://www.googleapis.com/auth/spreadsheets']
+
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(
+            CLIENT_SECRET_FILE, scopes=scopes)
+
         return credentials
 
     def idxtocol(self, idx):
@@ -127,7 +134,7 @@ def main():
     """
     spreadsheetid = '1U-wAQAXaDFYZ2uQvPtxL5kSDOss8kMPRRpyb6OgRbKs'
     ssupdator = HCSSUpdater(spreadsheetid)
-    ssupdator.update_score('Doug', str(1), str(int(time.time())))
+    ssupdator.update_score('Doug', str(2), str(int(time.time())))
     ssupdator.update_score('Derek', str(2), str(int(time.time())))
     ssupdator.update_score('Chad', str(3), str(int(time.time())))
 
