438ba14c96a6129e726c9cb7cee00f1283e66ea7
doug <doug@applegate.us>
2017-03-05 00:28:43 -0700

    Something's not working

--- hcssupdater.py
+++ hcssupdater.py
@@ -4,20 +4,10 @@ from __future__ import print_function
 from datetime import datetime
 
 import httplib2
-import os
 import time
 
 from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
 from oauth2client.service_account import ServiceAccountCredentials
-
-try:
-    import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
-except ImportError:
-    flags = None
 
 # If modifying these scopes, delete your previously saved credentials
 # at ~/.credentials/sheets.googleapis.com-python-quickstart.json
@@ -44,24 +34,6 @@ class HCSSUpdater(object):
         Returns:
             Credentials, the obtained credential.
         """
-        # home_dir = os.path.expanduser('~')
-        # credential_dir = os.path.join(home_dir, '.credentials')
-        # if not os.path.exists(credential_dir):
-        #     os.makedirs(credential_dir)
-        # credential_path = os.path.join(credential_dir,
-        #                                'sheets.googleapis.com-python-quickstart.json')
-        #
-        # store = Storage(credential_path)
-        # credentials = store.get()
-        # if not credentials or credentials.invalid:
-        #     flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        #     flow.user_agent = APPLICATION_NAME
-        #     if flags:
-        #         credentials = tools.run_flow(flow, store, flags)
-        #     else:  # Needed only for compatibility with Python 2.6
-        #         credentials = tools.run(flow, store)
-        #     print('Storing credentials to ' + credential_path)
-
         scopes = ['https://www.googleapis.com/auth/spreadsheets']
 
         credentials = ServiceAccountCredentials.from_json_keyfile_name(
