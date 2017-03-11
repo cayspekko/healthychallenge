0c224368ed1e2b9fe439212b64cd578f46f65da3
doug <doug@applegate.us>
2017-03-10 22:28:37 -0700

    Stats

--- main.py
+++ main.py
@@ -8,11 +8,16 @@ from hcssupdater import HCSSUpdater
 app = Flask(__name__)
 
 TEST_BOT_ID = '33284e04361b09285e04b5beb1'
-BOT_ID = "9f0b7f63622e7968c464b7ff8d"
+BOT_ID = '33284e04361b09285e04b5beb1'
+#BOT_ID = "9f0b7f63622e7968c464b7ff8d"
 BOT_URL = 'https://api.groupme.com/v3/bots/post'
 TEST_SHEET_ID = '1U-wAQAXaDFYZ2uQvPtxL5kSDOss8kMPRRpyb6OgRbKs'
 SHEET_ID = '15-w7N4Qqw5MnpRmnd7bm2cyqbgWR-JPOT-wnmpMnvNs'
 SHEET_LINK = 'https://goo.gl/HTWJLj'
+
+
+def bot_speak(text):
+    requests.post(BOT_URL, data={'bot_id': BOT_ID, 'text': text})
 
 
 def report_command(data):
@@ -20,15 +25,15 @@ def report_command(data):
         value = data['text'].split()[1]
     except (TypeError, IndexError, KeyError):
         logging.exception("error in report_command")
-        requests.post(BOT_URL, data={'bot_id': BOT_ID, 'text': "Sorry %s! That didn't work and I don't know what went wrong!" % (data['name'].split() or ['you'])[0]})
+        bot_speak("Sorry %s! That didn't work and I don't know what went wrong!" % (data['name'].split() or ['you'])[0])
         return
     updater = HCSSUpdater(SHEET_ID, sheet_name='Points')
     updater.update_score(data['name'], value, data['created_at'])
-    requests.post(BOT_URL, data={'bot_id': BOT_ID, 'text': "Okay %s! I added %s to the spreadsheet! %s" % ((data['name'].split() or ['you'])[0], value, SHEET_LINK)})
+    bot_speak("Okay %s! I added %s to the spreadsheet!" % ((data['name'].split() or ['you'])[0], value))
 
 
 def echo_command(data):
-    requests.post(BOT_URL, data={'bot_id': BOT_ID, 'text': " ".join(data['text'].split()[1:])})
+    bot_speak(" ".join(data['text'].split()[1:]))
 
 
 def quote_command(data):
@@ -36,7 +41,7 @@ def quote_command(data):
     data = r.json()
     try:
         quote = data['contents']['quotes'][0]
-        requests.post(BOT_URL, data={'bot_id': BOT_ID, 'text': '"%s" -%s' % (quote['quote'], quote['author'])})
+        bot_speak('"%s" -%s' % (quote['quote'], quote['author']))
     except (TypeError, IndexError, KeyError):
         logging.exception('quote_commend ran into an error')
 
@@ -47,19 +52,36 @@ def stats_command(data):
     response = []
     for i in range(len(stats[0])):
         response.append("%s: %s" % (stats[0][i], stats[1][i]))
-    requests.post(BOT_URL, data={'bot_id': BOT_ID, 'text': ", ".join(response)})
+    bot_speak(", ".join(response))
 
+
+def sheet_command(data):
+    bot_speak(BOT_URL)
+
+
+def help_command(data):
+    help = ['Here are the commands: ']
+    help.extend("{}{} {}".format(k, " ".join(v[1:-1]), v[-1]) for k, v in commands.items())
+    bot_speak("\n".join(help))
 
 commands = {
-    '/report': report_command,
-    '/echo': echo_command,
-    '/quote': quote_command,
-    '/stats': stats_command
+    '/report': (report_command, '[number]', 'also /r [number]. Report [number] to the spreadsheet.'),
+    '/echo': (echo_command, '[text]', 'Repeats [text].'),
+    '/quote': (quote_command, 'Gets the quote of the day courtesy theysaidso.com'),
+    '/stats': (stats_command, 'Gets the current report stats'),
+    '/sheet': (sheet_command, 'Prints the url of the spreadsheet'),
+    '/help': (help_command, 'Prints this help command')
+}
+
+short_commands = {
+    '/r': '/report'
 }
 
 
 def process_request(data):
-    commands.get((data['text'].split() or [None])[0], lambda x: None)(data)
+    command = (data['text'].split() or [None])[0]
+    command = short_commands.get(command, command)
+    commands.get(command, lambda x: None)(data)
 
 
 @app.route('/')
@@ -72,6 +94,7 @@ def hello():
 def groupme():
     if request.method == 'POST':
         json = request.get_json()
+        logging.critical('DATA: %s' % json)
         process_request({'text': json['text'], 'name': json['name'], 'created_at': json['created_at']})
         return ''
     else:
